5d2f8606647eefb124163f8ec62e70f10c186b1d
C1C0 <kpskola1002@gmail.com>
2020-01-21 17:12:10 +0100

    Add files via upload

---
+++ v_palovic_291219/Field.rb
@@ -0,0 +1,30 @@
+class Field
+    attr_accessor :coordinates, :state, :num
+    def initialize(x, y, state, num)
+        @coordinates = [x, y]
+        @state = state #0 - hidden, 1 - hidden+bombed, 2 - showed, 3- showed+empty,4 - flagged
+        @num = num
+
+        x = Array.new()
+
+        if @state == 0
+            self.defaultPrint()
+        end
+    end
+
+    
+    def defaultPrint
+        print "* "
+    end
+
+    public
+    def setNumPrint
+        if @state == 1
+            print "* "
+        elsif @state == 3
+            print "P "    
+        elsif @state == 2
+            print "#{@num} "        
+        end
+    end
+end
